e6f7f18d742ffb280a38f6ac83d43a14ebef4a1a
cz <598799959@qq.com>
2014-10-10 00:21:38 +0800

    add image

--- static/design/src/resource.js
+++ static/design/src/resource.js
@@ -1,12 +1,18 @@
 var s_HelloWorld = "res/HelloWorld.png";
 var s_CloseNormal = "res/CloseNormal.png";
 var s_CloseSelected = "res/CloseSelected.png";
+var s_small="res/small.png";
+var s_middle="res/middle.png";
+var s_sprite="res/sprite.png";
 
 var g_resources = [
     //image
     {src:s_HelloWorld},
     {src:s_CloseNormal},
-    {src:s_CloseSelected}
+    {src:s_CloseSelected},
+    {src:s_small},
+    {src:s_middle},
+    {src:s_sprite},
 
     //plist
 
