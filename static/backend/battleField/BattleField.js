8124a6513ff1231e669de39cbd5906d6b5b563fd
drinkwithwater <598799959@qq.com>
2015-03-09 02:19:42 +0800

    coding moving logic

--- static/backend/battleField/BattleField.js
+++ static/backend/battleField/BattleField.js
@@ -1,140 +1,163 @@
 module.exports=function(env){
-  ///{{{
-  var gBattle=env.gBattle=env.gBattle||{}
-  gBattle.BattleField=gUtil.Class.extend({
-    gameMaze:null,
-    interService:null,
-    //id to unit
-    unitDict:{},
-    // the trigger
-    moveTrigger:[],
-    attackTrigger:[],
-    failDict:{1:"path not continuous",
-      2:"path over length",
-      3:"path contain barrier",
-      4:"unit can't move",
-      5:"path illegal",
-      6:"unit not existed",
-      7:"destination valid"},
+    ///{{{
+    var gBattle=env.gBattle=env.gBattle||{}
+    gBattle.BattleField=gUtil.Class.extend({
+	    battleManager:null,
+	    gameMaze:null,
+    
+	    playerDict:{},
+    
+	    interService:null,
+	    //id to unit
+	    unitDict:{},
+	    // the trigger
+	    moveTrigger:[],
+	    attackTrigger:[],
+	    failDict:{1:"path not continuous",
+		      2:"path over length",
+		      3:"path contain barrier",
+		      4:"unit can't move",
+		      5:"path illegal",
+		      6:"unit not existed",
+		      7:"destination valid"},
+
+	    genPlayerId:function(){
+		//TODO
+	    },
+	    onPlayerJoin:function(battlePlayer,setId){
+		var newId=this.genPlayerId();
+		if(!newId){
+		    //TODO
+		}
+		battlePlayer.playerId=newId;
+		this.playerDict[newId]=battlePlayer;
+		//TODO broadcast playerin message
+	      
+	    },
+	    onPlayerExit:function(battlePlayer){
+		//TODO broadcast playerexit message
+		delete this.playerDict[battlePlayer.playerId];
+		if(_.size(this.playerDict)==0){
+		    this.battleManager.onBattleExit(this);
+		}
+
+	    },
+	    onPlayerMove:function(battlePlayer,unitId,path){
+		//TODO
+		var unit=this.unitDict[unitId];
+		if(!unit){
+		    //TODO
+		    //unit not existed error
+		    return;
+		}
+		if(unit.ownerId!=battlePlayer.playerId){
+		    //TODO
+		    //player cann't operator this unit
+		    return;
+		}
+		//TODO
+		//try to move , sendmessage back
+		////////////////
+		// move check //
+		////////////////
+		var checkResult=this.checkMovePath(unit,path);
+
+		////////////////
+		//move trigger//
+		////////////////
+		if(checkResult.success){
+		    var operResult=this.operMovePath(unit,path,checkResult);
+		    //TODO
+		    //message send success operate result
+		}else{
+		    //TODO
+		    //message send fail result
+		}
+	    },
+
+	    /**
+	     *  @return {success:1}|{fail:fail_num}
+	     */
+	    checkMovePath:function(unit,path){
+		//////////////////////////
+		//// unit vaild check ////
+		//////////////////////////
+		//check unit is existed
+		if(!unit){ return {fail:6} }
+		//check unit can move //unitMoveSkill is used in next step
+		var unitMoveSkill=unit.getMoveSkill();
+		if(unitMoveSkill===null) return {fail:4};
+		//check unit can be move by player
+		//TODO
+
+		//////////////////////////
+		//// path vaild check ////
+		//////////////////////////
+		// path continuous check
+		if(gUtil.continuous(path)){ return {fail:1} }
+		// path illegal check  //check by gameMaze
+		var cellPath=this.gameMaze.getCellPath(path);
+		if(cellPath===null) return {fail:5};
+		// destination valid
+		if(cellPath[cellPath.length-1].content===null){ return {fail:7}; }
+
+		//////////////////////////////////
+		//// unit can move path check ////
+		//////////////////////////////////
+		// range check
+		if(!unitMoveSkill.checkRange(path.length)) return {fail:2};
+		// each cell through check/*
+		var pathCheckResult=gUtil.allTrueIter(cellPath,function(cell){
+			return unitMoveSkill.checkCellThrough(cell);
+		    });*/
+		var pathCheckResult=_.every(unitMoveSkill,function(cellPath){
+			return unitMoveSkill.checkCellThrough(cell);
+		    });
+		if(pathCheckResult){ return {fail:3} }
+
+		//////////////////
+		//return success//
+		//////////////////
+		return {success:1, skill:unitMoveSkill};
+	    },
+	    /**
+	     *  @return {}
+	     */
+	    operMovePath:function(unit,path,result){
+		var skill=result.skill;
+		var moveTrigger=this.moveTrigger;
+		var operArray=[];
+		
+		//unit move
+		for(var j=0,l=path.length;j<l;j++){
+		    var pos=path[j];
+		    operArray.push(this.unitMove(unit,skill,pos));
+		    //unit trigger
+		    for(var i=0,l=moveTrigger.length;i<l;i++){
+			var trigger=moveTrigger[i];
+
+			if(trigger.checkArea(pos)){
+			    operArray.push(this.unitTrigger(unit,trigger));
+			    //TODO check dead
+			}
+		    }
+		}
+		return operArray;
+	    },
+
+	    ////////////////////////////////////////
+	    // the function for simple action TODO//
+	    ////////////////////////////////////////
+	    unitMove:function(unit,pos){
+	    },
+	    unitAttack:function(unit,skill,dstUnit){
+	    },
+	    unitTrigger:function(unit,trigger){
+	    },
 
 
-    /**
-     *  @return {success:1}|{fail:fail_num}
-     */
-    checkMovePath:function(unit,path){
-      if(typeof(unit)=="number") unit=unitDict[unit];
-
-      //////////////////////////
-      //// unit vaild check ////
-      //////////////////////////
-      //check unit is existed
-      if(!unit){ return {fail:6} }
-      //check unit can move //unitMoveSkill is used in next step
-      var unitMoveSkill=unit.getMoveSkill();
-      if(unitMoveSkill===null) return {fail:4};
-      //check unit can be move by player
-      //TODO
-
-      //////////////////////////
-      //// path vaild check ////
-      //////////////////////////
-      // path continuous check
-      if(gUtil.continuous(path)){ return {fail:1} }
-      // path illegal check  //check by gameMaze
-      var cellPath=this.gameMaze.getCellPath(path);
-      if(cellPath===null) return {fail:5};
-      // destination valid
-      if(cellPath[cellPath.length-1].content===null){ return {fail:7}; }
-
-      //////////////////////////////////
-      //// unit can move path check ////
-      //////////////////////////////////
-      // range check
-      if(!unitMoveSkill.checkRange(path.length)) return {fail:2};
-      // each cell through check
-      var pathCheckResult=gUtil.allTrueIter(cellPath,function(cell){
-        return unitMoveSkill.checkCellThrough(cell);
-      });
-      if(pathCheckResult){ return {fail:3} }
-
-      //////////////////
-      //return success//
-      //////////////////
-      return {success:1, skill:unitMoveSkill};
-    },
-    /**
-     *  @return {}
-     */
-    operMovePath:function(unit,path){
-      if(typeof(unit)=="number") unit=unitDict[unit];
-      ////////////////
-      // move check //
-      ////////////////
-      var result=this.checkMovePath(unit,path);
-
-      ////////////////
-      //move trigger//
-      ////////////////
-      if(result.success){
-        var skill=result.skill;
-        var showContext=new gInter.ShowContext();
-        //unit move
-        for(var j=0,l=path.length;j<l;j++){
-          var pos=path[j];
-          this.unitMove(showContext,unit,skill,pos);
-          //unit trigger
-          for(var i=0,l=moveTrigger.length;i<l;i++){
-            var trigger=moveTrigger[i];
-            if(trigger.checkArea(pos)){
-              this.unitTrigger(unit,trigger);
-              //TODO check dead
-            }
-          }
-        }
-        this.interService.show(showContext);
-        return {success:1}
-      }else{
-        //TODO
-        return result;
-      }
-    },
-
-    ////////////////////////////////////////
-    // the function for simple action TODO//
-    ////////////////////////////////////////
-    /**
-     * @param (gLogic.Unit) unit
-     * @param (gLogic.Skill) skill
-     * @param (point) pos
-     */
-    unitMove:function(showContext,unit,pos){
-    },
-    /**
-     * @param (gLogic.Unit) unit
-     * @param (gLogic.Skill) skill
-     * @param (point) dstUnit
-     */
-    unitAttack:function(showContext,unit,skill,dstUnit){
-    },
-    /**
-     * @param (gLogic.Unit) unit
-     * @param (point) direct
-     */
-    unitTurn:function(showContext,unit,direct){
-    },
-    /**
-     * @param (gLogic.Unit) unit
-     * @param (gLogic.Trigger) trigger
-     */
-    unitTrigger:function(showContext,unit,trigger){
-    },
-
-
-    //TODO
-    //include died, harmed
-    unitTODO:function(){}
-  }
-  //gLogic.GamePoolBattle.prototype=new gLogic.GamePoolInit();
-  //gLogic.LogicService.prototype=new gLogic.GamePoolBattle();
-  //}}}
-};
+	    //TODO
+	    //for e.g. died, harmed
+	    unitTODO:function(){}
+	}
+	};
--- static/backend/controller/BattleManager.js
+++ static/backend/controller/BattleManager.js
@@ -1,85 +1,94 @@
 module.exports=function(env){
-//{{{
-	var gController=env.gController=env.gController||{}
-  gController.BattlePlayer=gUtil.Class.extend({
-    session:null,
-    battle:null,
-    playId:null,//key in BattleField
-    userId:null,
-    constructor:function(gamePlayer){
-      this.session=gamePlayer.session;
-    }
-  });
-	gController.BattleManager=gUtil.Class.extend({
-    battleSet:{},
-		name:"battleModule",
-		serverModule:null,
-		init:function(gameTop){
-			this.serverModule=gameTop.getModule("serverModule");
-		},
-		start:function(gameTop){
-		},
-    //this is not called by ServerModule, but called by GameController
-		onMessage:function(gamePlayer,message){
-      var battlePlayer=this.getPlayer(gamePlayer);
-      var handlerFunc=this[this.messageHandlers[message.type]];
-      if(handlerFunc){
-        handlerFunc.call(this,battlePlayer,message);
-      }else{
-        console.error("message type error : "+message.type);
-      }
-      //TODO
-		},
-    messageHandlers:{
-      "start":"messageStart",
-      "join":"messageJoin",
-      "pathing":"messagePathing",
-      "exit":"messageExit",
-    },
-		messageStart:function(battlePlayer,message){
-      //if battle existed,remove.
-      if(battlePlayer.battle){
-        this.onClose(battlePlayer);
-      }
-      //create battleField
-      var battleField=gBattle.BattleFactory.createBattle(message.battleName);
-      this.battleSet[battleField._id]=battleField;
-      //set player.battle
-      battlePlayer.battle=battleField;
-      //reply client
-			var reMsg=new gMessage.SCStartScript();
-			this.serverModule.sendMessage(battlePlayer.session,reMsg);
-		},
-    messageJoin:function(){},
-    messagePathing:function(battlePlayer,message){
-      var battle=battlePlayer.battle;
-      if(battle){
-        //TODO
-      }else{
-        console.error("battle not started");
-      }
-    },
-    messageExit:function(battlePlayer,message){
-      this.onPlayerClose(battlePlayer);
-    },
-    //when player exit or player start a new battle
-		onPlayerExit:function(battlePlayer){
-      var battle=battlaPlayer.battle;
-      //TODO battle.onPlayerExit();
-      battlePlayer.battle=null;
-    },
-    onBattleExit:function(battle){
-    }
-    //convert player
-    sessionIdToPlayer:{},
-    getPlayer:function(gamePlayer){
-      var sidToPlayer=this.sessionIdToPlayer;
-      var battlePlayer=sidToPlayer[gamePlayer.session.id];
-      if(!battlePlayer){
-        battlePlayer=new gController.BattlePlayer(gamePlayer);
-      }
-      return battlePlayer;
-    }
+    //{{{
+    var gController=env.gController=env.gController||{}
+    gController.BattlePlayer=gUtil.Class.extend({
+	    session:null,
+	    battle:null,
+	    playerId:null,//key in BattleField
+	    userId:null,
+	    constructor:function(gamePlayer){
+		this.session=gamePlayer.session;
+	    }
 	});
-//}}}
+    gController.BattleManager=gUtil.Class.extend({
+	    battleSet:{},
+	    name:"battleModule",
+	    serverModule:null,
+	    init:function(gameTop){
+		this.serverModule=gameTop.getModule("serverModule");
+	    },
+	    start:function(gameTop){
+	    },
+	    //this is not called by ServerModule, but called by GameController
+	    onMessage:function(gamePlayer,message){
+		var battlePlayer=this.getPlayer(gamePlayer);
+		var handlerFunc=this[this.messageHandlers[message.type]];
+		if(handlerFunc){
+		    handlerFunc.call(this,battlePlayer,message);
+		}else{
+		    console.error("message type error : "+message.type);
+		}
+		//TODO
+	    },
+	    messageHandlers:{
+		"start":"messageStart",
+		"join":"messageJoin",
+		"pathing":"messagePathing",
+		"exit":"messageExit",
+	    },
+	    messageStart:function(battlePlayer,message){
+		//if battle existed,remove.
+		if(battlePlayer.battle){
+		    this.onPlayerExit(battlePlayer);
+		}
+		//create battleField
+		var battleField=gBattle.BattleFactory.createBattle(message.battleName);
+		this.battleSet[battleField._id]=battleField;
+		//set player.battle
+		battlePlayer.battle=battleField;
+		//reply client
+		var reMsg=new gMessage.SCStartScript();
+		this.serverModule.sendMessage(battlePlayer.session,reMsg);
+	    },
+	    messageJoin:function(){},
+	    messagePathing:function(battlePlayer,message){
+		var battle=battlePlayer.battle;
+		if(battle){
+		    //TODO
+		}else{
+		    console.error("battle not started");
+		}
+	    },
+	    messageExit:function(battlePlayer,message){
+		this.onPlayerExit(battlePlayer);
+	    },
+	    //when player exit battle or player start a new battle
+	    onPlayerExit:function(battlePlayer){
+		var battle=battlaPlayer.battle;
+		battle.onPlayerExit(battlePlayer);
+		//TODO battle.onPlayerExit();
+		battlePlayer.battle=null;
+	    },
+	    //when player connect close or fail
+	    onPlayerQuit:function(battlePlayer){
+		this.onPlayerExit(battlePlayer);
+		delete this.sessionIdToPlayer[battlePlayer.session.id];
+	    },
+	    onBattleExit:function(battle){
+		    
+		//remove from set
+		delete this.battleSet[battle._id];
+	    },
+	    //convert player
+	    sessionIdToPlayer:{},
+	    getPlayer:function(gamePlayer){
+		var sidToPlayer=this.sessionIdToPlayer;
+		var battlePlayer=sidToPlayer[gamePlayer.session.id];
+		if(!battlePlayer){
+		    battlePlayer=new gController.BattlePlayer(gamePlayer);
+		}
+		return battlePlayer;
+	    }
+	});
+    //}}}
 };
