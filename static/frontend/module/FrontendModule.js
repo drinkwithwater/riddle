b1f0382749978f6829dd445591fd3ef8b392ac00
cz <598799959@qq.com>
2015-03-19 17:42:55 +0800

    add fly path

--- static/frontend/module/FrontendModule.js
+++ static/frontend/module/FrontendModule.js
@@ -2,63 +2,64 @@ var gUI=gUI||{};
 var gTemplates=gTemplates||{};
 var htmlView;// this is used for debug
 gUI.FrontendModule=gUtil.Class.extend({
-  boardModel:null,
-  boardView:null,
-  cellCollection:null,
-  clientModule:null,
-  actionHandler:null,
+    boardModel:null,
+    boardView:null,
+    cellCollection:null,
+    clientModule:null,
+    actionHandler:null,
 
-  name:"frontendModule",
-  init:function(gameTop){
-      var thisVar=this;
-    if(gameTop){
-	this.clientModule=gameTop.getModule("clientModule");
-	this.clientModule.addListener(this);
-    }
-      var board=this.boardModel=new gModels.BoardModel();
-      var cells=this.cellCollection=new gModels.CellCollection();
-    this.loadTemplates(["cell","board"],function(){
-      thisVar.actionHandler=new gUI.ActionHandler(thisVar.clientModule);
-      thisVar.boardView=new gViews.BoardView({
-      	model:board,
-      	collection:cells,
-      	actionHandler:thisVar.actionHandler
-      });
-	//for test
-	htmlView=thisVar.boardView;
+    name:"frontendModule",
+    init:function(gameTop){
+        var thisVar=this;
+        if(gameTop){
+	        this.clientModule=gameTop.getModule("clientModule");
+	        this.clientModule.addListener(this);
+        }
+        var board=this.boardModel=new gModels.BoardModel();
+        var cells=this.cellCollection=new gModels.CellCollection();
+        this.loadTemplates(["cell","board"],function(){
+            thisVar.actionHandler=new gUI.ActionHandler(thisVar.clientModule);
+            thisVar.boardView=new gViews.BoardView({
+      	        model:board,
+      	        collection:cells,
+      	        actionHandler:thisVar.actionHandler
+            });
+	        //for test
+	        htmlView=thisVar.boardView;
 
-      cells.add(new gModels.CellModel({i:5,j:5}));//just try
-      $("#main").html(thisVar.boardView.render().el);
-      $("#start").on("click",function(){
-  	    thisVar.clientModule.sendMessage(new gMessage.CSStart());
-      });
-    });
-  },
-  start:function(gameTop){
-  },
+            cells.add(new gModels.CellModel({i:5,j:5}));//just try
+            $("#main").html(thisVar.boardView.render().el);
+            thisVar.boardView.afterRender();
+            $("#start").on("click",function(){
+  	            thisVar.clientModule.sendMessage(new gMessage.CSStart());
+            });
+        });
+    },
+    start:function(gameTop){
+    },
 
 
     
-  onMessage:function(message){
-  },
-  loadTemplates:function (names,callback){
-    var length=names.length;
-    var loadTemplate=function(index){
-      var name=names[index];
-      console.log("Loading template: "+name);
-      $.get("/frontend/templates/"+names[index]+".html",function(data){
-        gTemplates[name]=data;
-        console.log(data);
-        index++;
-        if(index<length){
-          loadTemplate(index);
-        }else{
-          callback();
+    onMessage:function(message){
+    },
+    loadTemplates:function (names,callback){
+        var length=names.length;
+        var loadTemplate=function(index){
+            var name=names[index];
+            console.log("Loading template: "+name);
+            $.get("/frontend/templates/"+names[index]+".html",function(data){
+                gTemplates[name]=data;
+                console.log(data);
+                index++;
+                if(index<length){
+                    loadTemplate(index);
+                }else{
+                    callback();
+                }
+            });
         }
-      });
+        loadTemplate(0);
     }
-    loadTemplate(0);
-  }
 },{
 	main:function(){
 		var game=gUI.frontendInst=new gUI.FrontendModule();
--- static/frontend/views/BoardView.js
+++ static/frontend/views/BoardView.js
@@ -1,78 +1,89 @@
 var gViews=gViews||{}
-gViews.WalkPath=gUtil.Class.extend({
-    STATE_EMPTY:0,
+gViews.FlyPath=gUtil.Class.extend({
+    STATE_EMTPY:0,
     STATE_PATHING:1,
-    CHECK_LEFT:1,
-    CHECK_RIGHT:2,
-    CHECK_UP:3,
-    CHECK_DOWN:4,
-    CHECK_EMPTY:5,//
-    CHECK_BREAK:6,// not continuous
     state:0,
     display:true,
     path:[],
     boardView:null,
     constructor:function(boardView){
         this.boardView=boardView;
+        this.state=this.STATE_EMPTY;
     },
     click:function(area){
         if(this.state==this.STATE_EMPTY){
-            this.state==this.STATE_PATHING;
+            this.state=this.STATE_PATHING;
             if(this.display){
-                //TODO 
-            }else{
-                //TODO 
             }
-        }else{
+            this.path.push(area);
+        }else if(this.state==this.STATE_PATHING){
             //TODO
             //send message
             //check path valid;
+            this.cancel(area);
+        }
+    },
+    over:function(area){
+    },
+    cancel:function(area){
+    },
+    openDisplay:function(area){
+    },
+    closeDisplay:function(area){
+    },
+    render:{
+    }
+});
+gViews.WalkPath=gUtil.Class.extend({
+    STATE_EMPTY:0,
+    STATE_PATHING:1,
+    state:0,
+    display:true,
+    path:[],
+    boardView:null,
+    constructor:function(boardView){
+        this.boardView=boardView;
+        this.state=this.STATE_EMPTY;
+        this.render.boardView=this.boardView;
+    },
+    click:function(area){
+        if(this.state==this.STATE_EMPTY){
+            this.state=this.STATE_PATHING;
+            if(this.display){
+                this.render.addRender(area,this.path);
+            }
+            this.path.push(area);
+        }else if(this.state==this.STATE_PATHING){
+            //TODO
+            //send message
+            //check path valid;
+            this.cancel(area);
         }
     },
     over:function(area){
         if(this.state==this.STATE_EMPTY){
             //do nothing
-        }else{
-            this.path.push(area);
+        }else if(this.state==this.STATE_PATHING){
             if(this.display){
-            }else{
+                this.render.addRender(area,this.path);
             }
+            this.path.push(area);
         }
     },
     openDisplay:function(){
+        this.display=true;
+        this.render.openRender(this.path);
     },
     closeDisplay:function(){
+        this.display=false;
+        this.render.closeRender(this.path);
     },
     cancel:function(area){
-        var allClass="path pathLeft pathRight pathTop pathBottom"
-        _.each(path,function(area){
-            this.boardView.area$(area).removeClass("allClass");
-        });
-    },
-    addRender:function(area,before){
-	    var end=_.last(this.before);
-        var direct=this.checkDirect(area);
-        var boardView=this.boardView;
-        if(direct==this.CHECK_RIGHT){
-            boardView.area$(area);
-            if(direct==pathing.CHECK_BREAK){
-	            this.cancelArea(area);
-	            return;
-            }else if(direct==pathing.CHECK_UP){
-	            this.area$(end).addClass("pathTop");
-	            this.area$(area).addClass("pathBottom");
-            }else if(direct==pathing.CHECK_DOWN){
-	            this.area$(end).addClass("pathBottom");
-	            this.area$(area).addClass("pathTop");
-            }else if(direct==pathing.CHECK_LEFT){
-	            this.area$(end).addClass("pathLeft");
-	            this.area$(area).addClass("pathRight");
-            }else if(direct==pathing.CHECK_RIGHT){
-	            this.area$(end).addClass("pathRight");
-	            this.area$(area).addClass("pathLeft");
-            }
-            this.area$(area).addClass("path");
+        if(this.display){
+            this.render.closeRender(this.path);
         }
+        this.path=[];
+        this.state=this.STATE_EMPTY;
     },
     //check if pathes contain area
     checkExisted:function(area){
@@ -84,23 +95,70 @@ gViews.WalkPath=gUtil.Class.extend({
         }
         return false;
     },
-    //check when pathing
-    checkDirect:function(area){
-	    var endArea=_.last(this.path);
-	    if(!endArea){
-	        return this.CHECK_EMPTY;
-	    }
-	    var di=area.i-endArea.i;
-	    var dj=area.j-endArea.j;
-	    if(di==0){
-	        if(dj==-1) return this.CHECK_RIGHT;
-	        if(dj==1) return this.CHECK_LEFT;
-	    }
-	    if(dj==0){
-	        if(di==1) return this.CHECK_UP;
-	        if(di==-1) return this.CHECK_DOWN;
-	    }
-	    return this.CHECK_BREAK;
+    render:{
+        CHECK_LEFT:1,
+        CHECK_RIGHT:2,
+        CHECK_UP:3,
+        CHECK_DOWN:4,
+        CHECK_EMPTY:5,//
+        CHECK_BREAK:6,// not continuous
+        boardView:null,
+        openRender:function(path){
+            var beforePath=new Array();
+            var render=this;
+            _.each(path,function(area){
+                render.addRender(area,beforePath);
+                beforePath.push(area);
+            });
+        },
+        closeRender:function(path){
+            var all="path pathLeft pathRight pathTop pathBottom";
+            var boardView=this.boardView;
+            _.each(path,function(area){
+                boardView.area$(area).removeClass(all);
+            });
+        },
+        //called when add an area
+        addRender:function(newArea,beforePath){
+            var endArea=_.last(beforePath);
+            var direct=this.checkDirect(newArea,endArea);
+            var boardView=this.boardView;
+            if(direct==this.CHECK_UP){
+                boardView.area$(endArea).addClass("pathTop");
+                boardView.area$(newArea).addClass("pathBottom");
+            }else if(direct==this.CHECK_DOWN){
+                boardView.area$(endArea).addClass("pathBottom");
+                boardView.area$(newArea).addClass("pathTop");
+            }else if(direct==this.CHECK_LEFT){
+                boardView.area$(endArea).addClass("pathLeft");
+                boardView.area$(newArea).addClass("pathRight");
+            }else if(direct==this.CHECK_RIGHT){
+                boardView.area$(endArea).addClass("pathRight");
+                boardView.area$(newArea).addClass("pathLeft");
+            }else if(direct==this.CHECK_EMPTY){
+                //do nothing
+            }else if(direct==this.CHECK_BREAK){
+                //just do this now;
+                //the path may be broken
+            }
+            boardView.area$(newArea).addClass("path");
+        },
+        checkDirect:function(dstArea,srcArea){
+	        if(!srcArea){
+	            return this.CHECK_EMPTY;
+	        }
+	        var di=dstArea.i-srcArea.i;
+	        var dj=dstArea.j-srcArea.j;
+	        if(di==0){
+	            if(dj==1) return this.CHECK_RIGHT;
+	            if(dj==-1) return this.CHECK_LEFT;
+	        }
+	        if(dj==0){
+	            if(di==-1) return this.CHECK_UP;
+	            if(di==1) return this.CHECK_DOWN;
+	        }
+	        return this.CHECK_BREAK;
+        }
     }
 });
 gViews.BoardView=Backbone.View.extend({
@@ -108,12 +166,17 @@ gViews.BoardView=Backbone.View.extend({
         "mousedown div.area.listener":"mouseDownArea",
         "mouseenter div.area.listener":"mouseEnterArea",
         "mouseleave div.area.listener":"mouseLeaveArea",
-        "mouseleave div.board":"mouseLeaveBoard"
+        "mouseleave div.board":"mouseLeaveBoard",
+        "mousemove div.boardChild.listener":"mouseMoveBoard"
     },
     actionHandler:{
     },
+    walkPath:null,
+    flyPath:null,
     constructor:function(aDict){
   	    gViews.BoardView.__super__.constructor.call(this,aDict);
+        this.walkPath=new gViews.WalkPath(this);
+        this.flyPath=new gViews.WalkPath(this);
         this.actionHandler=aDict.actionHandler||(new gUI.ActionHandler());
     },
     initialize:function(){
@@ -122,19 +185,42 @@ gViews.BoardView=Backbone.View.extend({
 
     area$:function(){
         var pos=gPoint.wrapArgs(arguments);
-        return this.$("tr#tr"+pos.i+" td#td"+pos.j+" div.area");
+        return this.$("tr#tr"+pos.i+" td#td"+pos.j+" div.area.viewer");
+    },
+    listenerArea$:function(){
+        var pos=gPoint.wrapArgs(arguments);
+        return this.$("tr#tr"+pos.i+" td#td"+pos.j+" div.area.listener");
+    },
+    viewerArea$:function(){
+        var pos=gPoint.wrapArgs(arguments);
+        return this.$("tr#tr"+pos.i+" td#td"+pos.j+" div.area.viewer");
+    },
+    line$:function(){
+        return this.$("#pathingline");
+    },
+    drawer$:function(){
+        return this.$(".boardChild.drawer svg");
     },
 
     render:function(msg){
         var self=this;
         var boardModel=this.model.toJSON();
         this.$el.html(this.template(boardModel));
+        // add cell
         _.each(this.collection.models,function(cell){
             var i=cell.get("i");
             var j=cell.get("j");
             self.$("tr#tr"+i+" td#td"+j+" div.area.viewer").html(new gViews.CellView({model:cell}).render().el);
         },self);
+
+        // set drawer width height
         return this;
+    },
+    afterRender:function(){
+        var width=this.$(".boardChild.listener").width();
+        var height=this.$(".boardChild.listener").height();
+        this.$(".boardChild.drawer svg").width(width);
+        this.$(".boardChild.drawer svg").height(height);
     },
 
     mouseDownArea:function(e){
@@ -143,12 +229,12 @@ gViews.BoardView=Backbone.View.extend({
             var i=$listener.attr("data-i");
             var j=$listener.attr("data-j");
             var area={i:i,j:j};
-            this.clickArea(area);
+            this.walkPath.click(area);
         }else if(e.button=2){
             var i=$listener.attr("data-i");
             var j=$listener.attr("data-j");
             var area={i:i,j:j};
-            this.cancelArea(area);
+            this.walkPath.cancel(area);
         }
     },
 
@@ -157,7 +243,7 @@ gViews.BoardView=Backbone.View.extend({
         var i=$listener.attr("data-i");
         var j=$listener.attr("data-j");
         var area={i:i,j:j};
-        this.overArea(area);
+        this.walkPath.over(area);
         this.area$(area).addClass("focus");
     },
 
@@ -170,8 +256,16 @@ gViews.BoardView=Backbone.View.extend({
     },
 
     mouseLeaveBoard:function(e){
-        this.cancelArea(null);
+        this.walkPath.cancel(null);
     },
+
+    mouseMoveBoard:function(e){
+        this.line$().attr("x2",e.pageX
+                          -this.drawer$().offset().left);
+        this.line$().attr("y2",e.pageY
+                          -this.drawer$().offset().top);
+    },
+    /*
 
     //detail operator
     pathing:{
@@ -283,6 +377,6 @@ gViews.BoardView=Backbone.View.extend({
             pathing.state=pathing.STATE_EMPTY;
         }else{
         }
-    }
+    }*/
 
 });
