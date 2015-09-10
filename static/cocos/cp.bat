d329ed34a368eb64014bef2c06cc126a1a2abdcf
cz <598799959@qq.com>
2015-09-10 15:57:08 +0800

    add cocos for js

---
+++ static/cocos/cp.bat
@@ -0,0 +1,3 @@
+cp -r src D:\work\cocos2d-x-3.7.1\tests\js-tests\
+cp main.js D:\work\cocos2d-x-3.7.1\tests\js-tests\main.js
+cp project.json D:\work\cocos2d-x-3.7.1\tests\js-tests\project.json
---
+++ static/cocos/src/resource.js
@@ -0,0 +1,10 @@
+var res = {
+    HelloWorld_png : "HelloWorld.png",
+    CloseNormal_png : "CloseNormal.png",
+    CloseSelected_png : "CloseSelected.png"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
